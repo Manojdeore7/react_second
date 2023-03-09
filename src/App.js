b40d6fd26b792cde6ecef704e072a238048f1b00
Manojdeore7 <manojddeore11>
2023-03-09 21:25:09 +0530

    add new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import ExpenseItems from "./components/ExpenseItems";
+function App() {
+  return (
+   <div>
+    <h2>Expense Itoms</h2>
+    <ExpenseItems></ExpenseItems>
+   </div>
+   
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,10 @@
+function ExpenseItems(){
+    return (
+ <div>
+    <h4>Food Rs 10</h4>
+    <h4>Petrol Rs 100</h4>
+    <h4>Movies Rs 200</h4>
+ </div>
+    );
+}
+export default ExpenseItems;
