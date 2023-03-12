90774c79960785005931cd0a4a79bed38c617d9b
Manojdeore7 <manojddeore11>
2023-03-12 18:47:03 +0530

    make components for good readability

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
-
 import ExpenseItems from "./components/ExpenseItems";
 function App() {
-
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location:"Toilet"
+      date: new Date(2020, 11, 14),
+      location: "Toilet",
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),location:"news" },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "news",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      location:"showroom"
+      location: "showroom",
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      location:"IT"
+      location: "IT",
     },
   ];
-   let arr=[];
+  let arr = [];
 
-  expenses.forEach((e)=>{
-   arr.push(<ExpenseItems title={e.title} date={e.date} amount={e.amount} LocationOfExpenditure={e.location} />)
-  })
-  
+  expenses.forEach((e) => {
+    arr.push(
+      <ExpenseItems
+        title={e.title}
+        date={e.date}
+        amount={e.amount}
+        LocationOfExpenditure={e.location}
+      />
+    );
+  });
+
   return (
-   <div>
-    <h2>Expense Itoms</h2>
-    
-   {arr};
-   </div>
-  
+    <div>
+      <h2>Expense Itoms</h2>
+      {arr};
+    </div>
   );
 }
 
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,17 +1,11 @@
-
-import "./ExpenseItem.css"
-
-function ExpenseItems(props){
-    return (
- <div className="expense-item">
-    <div >{props.date.toISOString()}</div>
-    <div className="expense-item__description">
-    <h2>{props.title}</h2>
-    <h2>{props.LocationOfExpenditure}</h2>
-    <div className="expense-item__price">${props.amount}</div>
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetail from "./ExpenseDetails";
+function ExpenseItems(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDetail title={props.title} amount={props.amount}></ExpenseDetail>
     </div>
-    
- </div>
-    );
+  );
 }
 export default ExpenseItems;
