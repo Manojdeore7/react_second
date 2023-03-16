9267bbf59f028a0a3085e7d984ad45a1a0caf103
Manojdeore7 <manojddeore11>
2023-03-16 08:50:06 +0530

    onclick added

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -2,10 +2,14 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetail from "./ExpenseDetails";
 import Card from "../UI/card";
 const ExpenseItems = (props) => {
+  let cliclHandler = () => {
+    console.log("clicked!!!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetail title={props.title} amount={props.amount}></ExpenseDetail>
+      <button onClick={cliclHandler()}>Click</button>
     </Card>
   );
 };
