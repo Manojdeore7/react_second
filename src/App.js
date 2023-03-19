221d02c906af6313ccd7440518cfa04c32e0aaa2
Manojdeore7 <manojddeore11>
2023-03-19 10:59:33 +0530

    filter added with some modification

--- src/App.js
+++ src/App.js
@@ -3,69 +3,64 @@ import ExpenseItems from "./components/Expenses/ExpenseItems";
 import Card from "./components/UI/card";
 import AddExpense from "./components/form/addExpense";
 import Ffilter from "./components/Expenses/Ffilter";
+import "./App.css";
+const Arr = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 11, 14),
+    location: "Toilet",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    location: "news",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: "showroom",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: "IT",
+  },
+];
 
 function App() {
-  let expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 11, 14),
-      location: "Toilet",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      location: "news",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: "showroom",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: "IT",
-    },
-  ];
+  const [expenses, setExpenses] = useState(Arr);
 
-  var [Arr, ArrFun] = useState(expenses);
-  var [arr, arrFun] = useState([]);
   const saveHandler = (data) => {
-    Arr.push(data);
-    arr = [];
-    Arr.forEach((e) => {
-      arr.push(
-        <ExpenseItems title={e.title} date={e.date} amount={e.amount} />
-      );
+    setExpenses((pre) => {
+      return [data, ...pre];
     });
-    arrFun(arr);
   };
+  let [filterYear, setFilterYear] = useState("");
+  let filterExpenses = expenses.filter((el) => {
+    if (filterYear == "") {
+      return el;
+    }
+    return el.date.getFullYear() == filterYear;
+  });
+  function onCheck(ele) {
+    setFilterYear(ele);
+  }
 
-  // Arr.forEach((e) => {
-  //   arr.push(<ExpenseItems title={e.title} date={e.date} amount={e.amount} />);
-  // });
-  function onCheck(e) {
-    let array = Arr.filter((el) => {
-      if (e == "") {
-        return el;
-      }
-      return el.date.getFullYear() == e;
-    });
-    arr = [];
-    array.forEach((e) => {
-      arr.push(
-        <ExpenseItems title={e.title} date={e.date} amount={e.amount} />
+  let content = <diV className="data">Threre is no data present</diV>;
+  if (filterExpenses.length > 0) {
+    content = filterExpenses.map((e, i) => {
+      return (
+        <ExpenseItems key={i} title={e.title} date={e.date} amount={e.amount} />
       );
     });
-    arrFun(arr);
   }
 
   return (
@@ -73,8 +68,11 @@ function App() {
       <h2>Expense Itoms</h2>
 
       <AddExpense onSaveData={saveHandler} />
-      <Ffilter check={onCheck}></Ffilter>
-      <Card className="manoj">{arr}</Card>
+
+      <Card className="manoj">
+        <Ffilter check={onCheck}></Ffilter>
+        {content}
+      </Card>
     </div>
   );
 }
--- src/components/Expenses/Ffilter.js
+++ src/components/Expenses/Ffilter.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import "./filter.css";
+import card from "../UI/card";
 function Ffilter(props) {
   function changeHander(em) {
     props.check(em.target.value);
   }
 
   return (
-    <div>
+    <card>
       <select onChange={changeHander} className="filter">
-        <option value="">filter the data</option>
+        <option value="">All Data</option>
         <option value="2000">2000</option>
         <option value="2021">2021</option>
         <option value="2022">2022</option>
+        <option value="2023">2023</option>
       </select>
-    </div>
+    </card>
   );
 }
 export default Ffilter;
